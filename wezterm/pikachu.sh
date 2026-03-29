65c828e75835698d6b34f66c72b86a679dd6796c
unknown <sonoda2468@gmail.com>
2026-03-29 18:26:13 +0900

    fix: ピカチュウの輪郭内のみを黄色にするように修正

--- wezterm/pikachu.sh
+++ wezterm/pikachu.sh
@@ -3,19 +3,10 @@ awk '
 {
     n = length($0)
 
-    # 各行の最初・最後の非スペース位置を検索
-    first = 0; last = 0
-    for (i = 1; i <= n; i++) {
-        if (substr($0, i, 1) != " ") {
-            if (first == 0) first = i
-            last = i
-        }
-    }
-
     out = ""
     for (i = 1; i <= n; i++) {
         c = substr($0, i, 1)
-        if (c == " " && i > first && i < last)
+        if (c == "Y")
             out = out "\033[48;5;226m \033[0m"
         else if (c ~ /[$qubdP]/)
             out = out "\033[38;5;232m" c "\033[0m"
@@ -30,4 +21,7 @@ awk '
     }
     print out
 }
+
+# 以下のリンクを参照
+# https://www.asciiart.eu/video-games/pokemon
 ' "$(dirname "$0")/pikachu.txt"
